ae5293dd2225b197834ae60c450dfda9fa8bc1b3
Sebastian Hård Samils <sebastian.samils@gmail.com>
2024-12-12 13:17:11 +0100

    yes

--- internal/handlers/homepage.go
+++ internal/handlers/homepage.go
@@ -6,10 +6,10 @@ import (
 )
 
 func HomePage(c *gin.Context) {
-	rendered, err := templates.ExamplePage()
-	if err != nil {
-		c.String(500, "Error rendering page: %v", err)
-		return
-	}
-	c.Data(200, "text/html; charset=utf-8", []byte(rendered))
+	rendered, err := templates.HomePage()
+	// if err != nil {
+	// 	c.String(500, "Error rendering page: %v", err)
+	// 	return
+	// }
+	// c.Data(200, "text/html; charset=utf-8", []byte(rendered))
 }
